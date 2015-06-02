392a4da95fcd3c956c09cbd94f3aeec30c2583ba
LUISantimateo <lsantimateo0393@outlook.com>
2015-06-01 21:53:38 -0500

    nombre = "luis santimateo" print (" Hola, " + nombre)

---
+++ sem2/quiz2.py
@@ -0,0 +1,6 @@
+Explique cuales son los modos de programacion en python 
+
+Modo interactivo:
+Cuando se leen órdenes desde una tty, se dice que el intérprete está en modo interactivo. En este modo, espera a la siguiente orden con el indicador principal, que suele se tres signos `mayor' (">>>" ). Para las líneas adicionales, se utiliza el indicador secundario, por omisión tres puntos ("... ").
+
+modo normal en este se pueden correr archivos .py 
